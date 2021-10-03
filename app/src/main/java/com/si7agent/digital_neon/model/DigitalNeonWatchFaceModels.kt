709dcb958caf71e23840454ff11d6ef8696d4ae5
Kirill Rudnev <kirilvasya@gmail.com>
2021-10-03 13:45:22 +0300

    created models templates

--- app/src/main/java/com/si7agent/digital_neon/model/DigitalNeonWatchFaceModels.kt
+++ app/src/main/java/com/si7agent/digital_neon/model/DigitalNeonWatchFaceModels.kt
@@ -1,9 +1,32 @@
 package com.si7agent.digital_neon.model
 
 data class DigitalNeonWatchFaceStyle(
-    val font: String,
-    val second: Int,
-    val theme: Int
+    val font: WatchFaceFont,
+    val theme: WatchFaceTheme
+)
+
+data class WatchFaceTheme(
+    val labelColor: Int,
+    val themeNamePic: String
+)
+
+data class WatchFaceFont(
+    val hourSize: Int,
+    val minuteSize: Int,
+    val labelSize: Int,
+    val fontName: String
+)
+
+data class WatchFaceDimensions(
+    val hourHandRadiusRatio:Float,
+    val minuteHandRadiusRatio:Float,
+    val secondHandRadiusRatio:Float,
+    val hourHandWidth:Float,
+    val minuteHandWidth:Float,
+    val secondHandWidth:Float,
+    val shadowRadius:Float,
+    val innerCircleRadius:Float,
+    val innerCircleToArmsDistance:Float
 )
 
 data class WatchFaceBackgroundImage(val backgroundImageResource: Int) {
